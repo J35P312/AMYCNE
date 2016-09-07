2e1d38d8fe1ecfcafb415ff553fc4ba34d74137d
J35P312 <jesper.eisfeldt@scilifelab.se>
2016-09-07 17:42:56 +0200

    new file:   100_GC_hg19.tab 	new file:   AMYCNE_deletion_filter.py 	new file:   NA12878.DEL.vcf 	new file:   NA12878_100bp.tab.tab 	new file:   NA12878_500.db 	new file:   README.md 	new file:   VCF_intra_size_filter.py 	new file:   annotated_deletions.vcf 	new file:   benchmarking_NA.sh 	new file:   benchmarking_NA.sh~ 	new file:   cleanVCF.py 	new file:   cnv_filtered_del.vcf 	new file:   query/query_db.py 	new file:   query/readVCF.py 	new file:   query/readVCF.pyc

---
+++ benchmarking_and_examples/delly_filtering/benchmarking_NA.sh
@@ -0,0 +1,19 @@
+#first argument: the vcf path
+#second argument: the name of the caller
+echo "the number of validated deletions:"
+echo wc -l NA12878_500.db
+echo "the number of hits:"
+python query/query_db.py --no_var --variations $1 --files NA12878_500.db > $2_query.vcf
+grep "OCC=1" $2_query.vcf | wc -l
+echo "the number of false call deletions(larger han 500bp):"
+python VCF_intra_size_filter.py --vcf $2_query.vcf > $2_size_filt.vcf
+grep "DEL" $2_size_filt.vcf > $2_size_filt_deletions.vcf
+grep "OCC=0" $2_size_filt_deletions.vcf | wc -l
+
+echo " the number of hits after using the filter tag as filter:"
+python cleanVCF.py --vcf $2_query.vcf > $2_qual.vcf
+grep "OCC=1" $2_qual.vcf | wc -l
+echo "the number of false called deletions after filtering:"
+python VCF_intra_size_filter.py --vcf $2_qual.vcf > $2_size_filt_qual.vcf
+grep "DEL" $2_size_filt_qual.vcf > $2_size_filt_deletions_qual.vcf
+grep "OCC=0" $2_size_filt_deletions_qual.vcf | wc -l

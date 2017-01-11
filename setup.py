f64cd843f11a056e2fb8e0a807cf05062149c163
Jesper Eisfeldt <J35P312@users.noreply.github.com>
2017-01-11 11:49:04 +0100

    Update setup.py
    forgot to upload the chromosome count module ....

--- setup.py
+++ setup.py
@@ -3,5 +3,5 @@ from Cython.Build import cythonize
 
 setup(
   name = 'AMYCNE',
-  ext_modules = cythonize(["common.py","call.py","count.py"]),
+  ext_modules = cythonize(["common.py","call.py"]),
 )
